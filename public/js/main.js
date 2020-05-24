b3306f0d53b56a1f8c13c92bc44097504a291091
50UM3N <soumen2015.s.k@gmail.com>
2020-05-24 18:44:04 +0530

    complete basic structure of the weather app

--- public/js/main.js
+++ public/js/main.js
@@ -30,29 +30,15 @@ imp.addEventListener('input', function (e) {
                 <svg  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"> <g>  <g><path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719 c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z" /></g></g></svg>
                 <strong>${data[i].name.slice(0, d.length)}</strong>${data[i].name.slice(d.length)}
                 <span > ${data[i].country}</span>
-                <input type="hidden" value="${data[i].name}">
+                <input type="hidden" value="${data[i].name},${data[i].lat},${data[i].lng}">
             `
-                b.addEventListener('click', function () { imp.value = this.getElementsByTagName('input')[0].value; });
+                b.addEventListener('click', function () { imp.value = this.getElementsByTagName('input')[0].value.split(',')[0]; addweather(this.getElementsByTagName('input')[0].value); });
                 a.appendChild(b);
             }
+            // addweather(data);
         })
         .catch(err => console.log('somthing error ' + err));
-    /*for (let i = 0;  i< cities.length; i++) {
-        if (cities[i].name.slice(0, this.value.length).toLocaleLowerCase() == this.value.toLocaleLowerCase() && c < 6) {
-            b = document.createElement("div");
-            b.innerHTML = "<strong>" + cities[i].name.slice(0, this.value.length) + "</strong>" + cities[i].name.slice(this.value.length) + "<input type='hidden' value='" + cities[i].name + "'>";
-            b.addEventListener("click", function (e) {
-                imp.value = this.getElementsByTagName('input')[0].value;
-            });
-            a.appendChild(b);
-            c++;
-        }
-        else if (c>=5){
-            console.log("break");
-            break;
-        }
-    }
-}*/}
+}
 );
 imp.addEventListener('keydown', (e) => {
     let x = document.getElementById('aslist');
@@ -66,7 +52,10 @@ imp.addEventListener('keydown', (e) => {
         focus(x);
     }
     else if (e.keyCode == 13) {//enter
-        if (current > -1) x[current].click();
+        if (current > -1) {
+            x[current].click();
+            addweather(x[current].getElementsByTagName('input')[0].value);
+        }
     }
 });
 function focus(x) {
@@ -86,3 +75,37 @@ function deleteall() {
     if (d != null) d.remove();
 }
 document.addEventListener('click', (e) => { if (e.target != document.getElementById('inp')) deleteall(); });
+
+function addweather(data) {
+    fetch('/weather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            lat: data.split(',')[1],
+            lng: data.split(',')[2]
+        })
+    })
+        .then(res => {
+            if (res.status >= 200 && res.status < 300)
+                return Promise.resolve(res.json());
+            else
+                return Promise.reject(new Error(res.statusText));
+        })
+        .then(data => {
+            setdata(data);
+        })
+}
+function setdata(data) {
+    document.getElementById('temp').innerHTML = "Tempreature: " + data.currently.temperature;
+    document.getElementById('atemp').innerHTML = "Apparent Tempreature: " + data.currently.apparentTemperature;
+    document.getElementById('dp').innerHTML = "Dew Point: " + data.currently.dewPoint;
+    document.getElementById('hd').innerHTML = "Humidity: " + data.currently.humidity;
+    document.getElementById('pre').innerHTML = "Pressure: " + data.currently.pressure;
+    document.getElementById('wp').innerHTML = "Wind Speed: " + data.currently.windSpeed;
+    document.getElementById('ui').innerHTML = "UV Index: " + data.currently.uvIndex;
+    document.getElementById('oz').innerHTML = "Ozone: " + data.currently.ozone;
+
+}
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const d = require('./cities');
+require('dotenv').config();
+const DARKSKY_API = process.env.DARKSKY_API;
 app.use(express.static('public'));
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, (e) => {
-    console.log('server started' + port);
+    console.log('server started ' + port);
 });
 app.use(express.json());
 app.post('/', (req, res) => {
@@ -17,4 +20,17 @@ app.post('/', (req, res) => {
         else if (c >= 5) break;
     }
     res.json(a);
-})
+});
+app.post('/weather', (req, res) => {
+    const url = `https://api.darksky.net/forecast/${DARKSKY_API}/${req.body.lat},${req.body.lng}`;
+    fetch(url)
+        .then(response => {
+            if (response.status >= 200 && response.status < 300)
+                return Promise.resolve(response.json());
+            else
+                return Promise.reject(new Error(response.statusText));
+        })
+        .then(data => {
+            res.json(data);
+        })
+});
